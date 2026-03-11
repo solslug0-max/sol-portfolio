1276613d1add1bbd62088edc816d8bc69b96a653
solslug0-max <SOLSLUG0@GMAIL.COM>
2026-03-11 17:55:17 -0500

    Hide Testimonials section (placeholder data, preserved for future use)

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <Experience />
       <Skills />
       <Education />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Contact />
       <WhatsAppBubble />
     </div>
